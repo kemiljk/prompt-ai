cbafce2ec3dbc7bc14a242a21136128b748daf4b
Karl Koch <karl@kejk.tech>
2022-12-14 20:25:43 +0000

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Karl Koch on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
